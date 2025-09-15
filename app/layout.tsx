ca70cc4f76757da6edfb157727891bd59906f17d
kayp514 <pa.ka12@outlook.com>
2025-09-15 22:35:30 +0000

    fix: update TernSecureProvider to use browserCookie persistence

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TernSecureProvider requiresVerification={true}>
+        <TernSecureProvider persistence="browserCookie">
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
