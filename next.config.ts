3e9529c59ea18f777e838974ba899afc1e7c059d
kayp514 <pa.ka12@outlook.com>
2025-09-18 16:46:43 +0000

    feat: add headers and rewrites configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,44 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/auth/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://tern.com" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/__/",
+        destination: "/",
+      },
+      {
+        source: "/__/auth/:path*",
+        destination:
+          "https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*",
+      },
+      {
+        source: "/__/firebase/:path*",
+        destination:
+          "https://dev-coffeeconnect-v1.firebaseapp.com/__/firebase/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
