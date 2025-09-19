75ff73414abab1248ebd116436dbc54199eae3ec
kayp514 <pa.ka12@outlook.com>
2025-09-19 13:45:34 +0000

    feat: add rewrite rule for root path and update @tern-secure/nextjs to latest canary version

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/__/",
+        destination: "/",
+      },
+      {
         source: "/__/auth/:path*",
         destination:
           "https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*",
