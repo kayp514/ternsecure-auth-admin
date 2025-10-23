a0cf1cbff147356e4d5da80fa354ea81bd0bb43d
kayp514 <pa.ka12@outlook.com>
2025-10-23 03:51:23 +0000

    feat: enhance SignInFormField with email/password handling and integrate SignInProvider

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,13 @@
-'use client'
+
 
 import { SignInFormField } from "@/components/signin-form-field";
 
+import { SignInProvider} from '@tern-secure/nextjs';
+
 export default function Page() {
-  return <SignInFormField />
+  return (
+    <SignInProvider>
+      <SignInFormField />
+    </SignInProvider>
+  );
 }
