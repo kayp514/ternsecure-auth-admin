25de6ee694668611aa95e4c8e363bc44c12070a4
kayp514 <pa.ka12@outlook.com>
2025-11-02 13:28:10 +0000

    fix: change Google sign-in mode from redirect to popup feat: add rewrites for Firebase authentication in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,21 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination:
+          "https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*",
+      },
+      {
+        source: "/__/firebase/:path*",
+        destination:
+          "https://dev-coffeeconnect-v1.firebaseapp.com/__/firebase/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
