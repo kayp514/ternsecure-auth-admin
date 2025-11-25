ea8dfd007a2f0667a543a3a73938271431c10521
kayp514 <pa.ka12@outlook.com>
2025-11-25 19:09:18 +0000

    fix: update appCheck site key in TernSecureProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       apiUrl="ternsecure-auth-admin.vercel.app"
       appCheck={{
         provider: "reCaptchaEnterprise",
-        siteKey: "6LfzGRgsAAAAAGEvbwbcLgT4IHWmuWv4kEDRA5hi",
+        siteKey: "6LcSGxgsAAAAACeeo12X2kYaO_VcsWZb8gzmfLRq",
         isTokenAutoRefreshEnabled: true,
       }}
       ternUIUrl="https://cdn.jsdelivr.net/npm/@tern-secure/auth@1.1.0-canary.v20251125170702/dist/ternsecure.browser.js"
