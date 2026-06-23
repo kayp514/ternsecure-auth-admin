1c1dca4e4c4c166755b430859a9667fff4eb98a7
kayp514 <pa.ka12@outlook.com>
2026-06-23 14:35:31 +0000

    fix: update ternUIUrl to latest version for TernSecure integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         isTokenAutoRefreshEnabled: true,
       }}
       apiUrl="ternsecure-auth-admin.vercel.app"
-      ternUIUrl="https://cdn.jsdelivr.net/npm/@tern-secure/auth@1.1.0-canary.v20251206170928/dist/ternsecure.browser.js"
+      ternUIUrl="https://cdn.jsdelivr.net/npm/@tern-secure/auth@1.1.0-canary.v20260623135837/dist/ternsecure.browser.js"
       persistence="browserCookie"
     >
       <ThemeProvider
